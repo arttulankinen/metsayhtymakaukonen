2b0022f92720efff288f4cdb70cb17132d2dcff0
arttulankinen <arttu.lankinen@edu.savonia.fi>
2025-09-26 11:53:38 +0300

    update

--- Backend/Sender.js
+++ Backend/Sender.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use("/api/email", emailRoutes);
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname,"..", "frontend", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname,"..","frontend", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
