5dd5950e0624de3391521fc5f854694e5773d56a
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2025-11-14 14:14:07 +0200

    Change default PORT from 5000 to 10000

--- Sender.js
+++ Sender.js
@@ -7,7 +7,7 @@ const emailRoutes = require("./Routes/emailRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
